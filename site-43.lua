049f7cec0079bafbbe6028cce426fdb25a4a81a5
prostone <151870524+prostone1@users.noreply.github.com>
2025-12-05 19:06:09 +0100

    Update site-43.lua

--- site-43.lua
+++ site-43.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/3a8a4735f0722830e31ecca117257ccec38377fbee159da67afce0104388d300/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8252f0582ac4d87309b8db284279e43301752be9746caef0c913023a726babd4/download"))()

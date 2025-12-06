78953fe0e9c01be04529fe395f43a167c6022aa4
prostone <151870524+prostone1@users.noreply.github.com>
2025-12-06 16:46:18 +0100

    Update school shibuya.lua

--- school shibuya.lua
+++ school shibuya.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/c4883c2e03387823e3d0f4c4c58cb88a2ad2f0d0179381e690bee20668997cef/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/e194e3a7d6a56a08a8a3b157b2e223e7dc0341be6ff69efd1c90ce37a775bd15/download"))()

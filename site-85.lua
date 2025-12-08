832061bf999a1efb116ef96839dcd70e75526955
prostone <151870524+prostone1@users.noreply.github.com>
2025-12-08 22:06:46 +0100

    Update site-85.lua

--- site-85.lua
+++ site-85.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/55dfad551e04204b1fadda197192dfe4259758c6396393af8ae877647f6c0a87/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/1104d3900cb8152fab1b1db66d875b78b3ec42c110f09fede9837f83b4589793/download"))()
